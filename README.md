45be41adf39fd3a9a02bb69d201bd3d9a8818305
Andrew Lamoureux <andrew@vector35.com>
2019-04-05 14:08:53 -0400

    tool to compile all formats

--- README.md
+++ README.md
@@ -7,4 +7,4 @@ tests and toys related to kaitai struct
 * pip put it in /usr/local/lib/python3.7/site-packages/kaitaistruct.py
 * recommend you clone https://github.com/kaitai-io/kaitai_struct_python_runtime.git
 * `python setup.py build` and `python setup.py install` put /usr/local/lib/python3.7/site-packages/kaitaistruct-0.9-py3.7.egg
-* the python modules compiled from your kaitai formats with debug are in your python path
+* the python modules compiled from your kaitai formats with debug are in your python path, see `compile.py`
---
+++ compile.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+
+import os
+
+if __name__ == '__main__':
+	HOME = os.environ['HOME']
+
+	path_formats = os.path.join(HOME, 'repos/lwerdna/kaitai_struct_formats')
+	path_compiler = os.path.join(HOME, 'Downloads/kaitai_struct_compiler/jvm/target/universal/stage/bin/kaitai-struct-compiler')
+
+	import_statements = []
+
+	for folder, subfolder, fnames in os.walk(path_formats):
+		if folder.endswith('.git'):
+			continue
+		if folder.endswith('.circleci'):
+			continue
+		if folder.endswith('_build'):
+			continue
+
+		# compile each
+		for fname in fnames:
+			if not fname.endswith('.ksy'):
+				continue
+			if fname in exceptions:
+				continue
+
+			path_ksy = os.path.join(folder, fname)
+			
+			cmd = '%s --debug --target python --import-path %s --outdir %s %s' % \
+				(path_compiler, path_formats, folder, path_ksy)
+			print(cmd)
+			os.system(cmd)
+
